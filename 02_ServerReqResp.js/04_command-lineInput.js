d037c692828c14221a93de980f32f1ecff11cf52
unknown <shubh386a@gmail.com>
2026-01-31 15:23:25 +0530

    add command-line input handling and HTTP server setup examples

---
+++ 02_ServerReqResp.js/04_command-lineInput.js
@@ -0,0 +1,14 @@
+// +++++++++++++++++++++ How to get Input form the terminal ++++++++++++++++++++++++++++++++++++
+const arg= process.argv;
+console.log('------',arg);// these are return the array and in this where node is install , current path , than 
+// input that are inter in Terminal 
+ //[
+//   'E:\\node.exe',
+//   'D:\\node-js2\\02_ServerReqResp.js\\04_command-lineInput.js',
+//   'shubham',
+//   'kushwaha'
+// ]
+// these are 
+
+
+console.log(arg[3])// kushwaha 
---
+++ 02_ServerReqResp.js/05_test_04.js
@@ -0,0 +1,9 @@
+const http= require('http');
+const arg= process.argv;
+    const portid=arg[2];
+http.createServer((req,resp)=>{
+    
+    resp.write("testing input cmd on the posrt "+portid)
+    resp.end();
+
+}).listen(portid)
