2464b8e9a384436235ee422a970e3b2c56ece31b
unknown <shubh386a@gmail.com>
2026-02-02 01:49:47 +0530

    Add initial documentation for Node.js concepts: Call stack, Node APIs, and Event Loop

---
+++ 03_SynAsy/02_HowToNodeWork.js
@@ -0,0 +1,14 @@
+//there are there block where 
+//01 . Call stack:-
+                  // in this only accesing functioncall by defult main() are accesing in there
+        
+//02.  Node APIs :-
+                // setTimeout is the feture of Node js Api   
+                // sbse pahle setTimeOut node API me jata hai 
+                // fir vha pe time complete hone ke baad 
+                // Event loop me jata hai uske baad istigar krta hai ki callStack khali hai ya nhi 
+
+
+//03. Event Loop :-(CallBack QUEUE)
+                   //jo function Node Api se aa jati hai unko se redy rakhta hai call stack me dalne ke liye 
+                   // Isme se function call stack me tb jate hai jb call stack khali ho jata hai... 
