b8bc5dc2c9955bd7a7257400ab008ac9bd891d1e
unknown <shubh386a@gmail.com>
2026-02-06 19:45:16 +0530

    Enhance middleware explanation with real-life examples; add Express.js server setup and routes for home, users, and products

--- 10_Middleare/01_whatIs.js
+++ 10_Middleare/01_whatIs.js
@@ -25,3 +25,52 @@
 // Serve static files
 // Handle errors
 // Protect routes
+
+//+++++++++++++++++++++++++++++ Real-life Example +++++++++++++++++++++++++++
+
+
+// Think of middleware like security checks at an airport ✈️
+// 1. ID check
+// 2. Luggage scan
+// 3. Ticket verification
+// 4. Boarding gate
+// 5. Each step is middleware.
+// If you fail one → you stop.
+
+import express from 'express'
+import path from 'path'
+
+const app=express();
+const absPath = path.resolve('./html')
+
+// function checkRoute (req,resp,next){ // this is the simlest way to create middleware .. in that we accessing 
+    // some parameters like :- req,resp and next -> this is use for the what do next ...
+//     console.log(`User access to this ${req.url} page`);
+//     next();
+// }
+
+// app.use(checkRoute); // this is the way to call middleware ...
+
+//+++++++++++++++ Best method to call middleware ++++++++++++++++
+
+ app.use((req,resp,next)=>{
+       console.log(`User access to this ${req.url} page`);
+       next();
+ })
+
+
+
+
+app.get('/',(req,resp)=>{
+     resp.sendFile(absPath +'/home.html')
+})
+
+app.get('/users',(req,resp)=>{
+    resp.send("<h1>User simple page</h1>")
+})
+
+app.get('/products',(req,resp)=>{
+    resp.send("<h1>Products .........</h1>")
+})
+
+app.listen(4800);
